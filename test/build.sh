22c0d98b29eef63a6f70610064d7cd3b6975082e
Dr Nic Williams <drnicwilliams@gmail.com>
2017-12-08 01:50:27 -0800

    initial mkdocs scaffold; borrowed from ultimate-guide-to-bosh

--- test/build.sh
+++ test/build.sh
@@ -3,28 +3,28 @@
 set -e
 # set -x
 
-./01_*/run.sh
-./02_*/run.sh
-./03_*/run.sh
-./06_*/run.sh
-./07_*/run.sh simple
-./07_*/run.sh renamed
-./08_*/run.sh
-./09_*/run.sh ls-abc-xyz
-./09_*/run.sh ls-abc
-./09_*/run.sh pretty-ls
-./10_*/run.sh
+./tutorials/01_*/run.sh
+./tutorials/02_*/run.sh
+./tutorials/03_*/run.sh
+./tutorials/06_*/run.sh
+./tutorials/07_*/run.sh simple
+./tutorials/07_*/run.sh renamed
+./tutorials/08_*/run.sh
+./tutorials/09_*/run.sh ls-abc-xyz
+./tutorials/09_*/run.sh ls-abc
+./tutorials/09_*/run.sh pretty-ls
+./tutorials/10_*/run.sh
 
 # # ./12_*/run.sh credentials.yml
 
-./20_*/run.sh credentials.yml get-version
-./20_*/run.sh credentials.yml display-version
-./20_*/run.sh credentials.yml rename-resource
-./20_*/run.sh credentials.yml bump-minor
-./20_*/run.sh credentials.yml bump-rc-save
-./20_*/run.sh credentials.yml bump-save-no-trigger
+./tutorials/20_*/run.sh credentials.yml get-version
+./tutorials/20_*/run.sh credentials.yml display-version
+./tutorials/20_*/run.sh credentials.yml rename-resource
+./tutorials/20_*/run.sh credentials.yml bump-minor
+./tutorials/20_*/run.sh credentials.yml bump-rc-save
+./tutorials/20_*/run.sh credentials.yml bump-save-no-trigger
 
-./35_*/run.sh credentials.yml
-./36_*/run.sh credentials.yml
+./tutorials/35_*/run.sh credentials.yml
+./tutorials/36_*/run.sh credentials.yml
 
-./40_*/run.sh credentials.yml
+./tutorials/40_*/run.sh credentials.yml
